2c381b7d9ab4bebac2822476f0753018aa60514c
vesmaluza <vesma.luza@svg.lv>
2022-03-18 11:50:46 +0200

    papildinats JS, pievienoti txt un json faili

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log(1)
+//console.log(2)
+//izsaucot funkcju, tiks nosutita zina
+function sutitZinu() {
+    console.log('sutitZinu() darbojas');
+    //lai pārbaudītu vai konsolē strādā, uzraksta f-jas nosaukumu
+    let zina = document.querySelector('.manaZina');//pieklust laukumam,kur var rakstit zinu
+    //mainigaja Zina būs vērtība, kas ierakstīta laukumā
+    //value nevajag,jo tad nevar dabut aktualo no laukuma
+    let zinas = document.querySelector('.chataZinas');//visas zinas, kas atsauksies uz chataZinas elementu
+    zinas.innerHTML = zinas.innerHTML+'<br>'+zina.value; 
+    //ar br piepluso jauno zinu
+    //tas, kas ir laukumina, ieviet innerHTML
+    //div elementam mainis ieksejo html
+}
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('chatazinas.txt');
+    let dati = await datiNoServera.text();
+    zinas.innerHTML = dati;
+}
+//saja bridi izveido ari txt failu
+  //pasaka, ka sanemtos datus no servera redzes ka tekstu
+//asycn nozime, ka f-ja notiek nesinhronas lietas
+//fecth lauj piekluut kadam failam un iesaukt tā saturu ieksā
+
+setInterval(ieladetChataZinas,1000) 
+//reizi sekundē izpildīt f-ju ieladetChataZinas()
